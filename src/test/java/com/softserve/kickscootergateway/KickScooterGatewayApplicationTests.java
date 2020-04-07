a28912acf7fad07ecf4bb14567bad95925896cc2
Edgar <edgarkrit@gmail.com>
2020-04-07 12:10:29 +0300

    cloud gateway configured via yml

---
+++ src/test/java/com/softserve/kickscootergateway/KickScooterGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserve.kickscootergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickScooterGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
